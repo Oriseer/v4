af4a192a33b508980eccd1df33059883c704a1da
Oriseer <74477729+Oriseer@users.noreply.github.com>
2021-04-18 13:02:50 +0800

    Add files via upload

---
+++ psignum/db.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Credentials
+    $servername = "localhost";
+    $username = "root";   //put your phpmyadmin username.(default is "root")
+    $password = "";     //if your phpmyadmin has a password put it here.(default is "root")
+    $dbname = "phploginapp";
+
+  // Attempt to connect to MySQL
+
+   $conn = mysqli_connect($servername, $username, $password, $dbname);
+  if ($conn->connect_error) {
+        die("Database Connection failed: " . $conn->connect_error);
+    }
---
+++ psignum/register.php
@@ -0,0 +1,146 @@
+<?php
+  // Include db config
+  require_once 'db.php';
+
+  // Init vars
+  $name = $email = $password = $confirm_password = '';
+  $name_err = $email_err = $password_err = $confirm_password_err = '';
+
+  // Process form when post submit
+  if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    // Sanitize POST
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+    // Put post vars in regular vars
+    $name =  trim($_POST['name']);
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $confirm_password = trim($_POST['confirm_password']);
+
+    // Validate email
+    if(empty($email)){
+      $email_err = 'Please enter email';
+    } else {
+      // Prepare a select statement
+      $sql = 'SELECT id FROM users WHERE email = ?';
+
+      $result = mysqli_stmt_init($conn);
+      if (!mysqli_stmt_prepare($result, $sql)) {
+        echo "SQL_Error_Select_device";
+        exit();
+    }
+    else{
+        mysqli_stmt_bind_param($result, "s", $email);
+        mysqli_stmt_execute($result);
+        $resultl = mysqli_stmt_get_result($result);
+        // Attempt execute
+        if($row = mysqli_fetch_assoc($resultl)){
+          // Check if email exists
+          if($row > 1){
+            $email_err = 'email already taken';
+          }
+        }
+         /* else {
+          echo "Something went wrong";
+          // Close statement
+          exit();
+        }*/
+      }
+    }
+
+    // Validate name
+    if(empty($name)){
+      $name_err = 'Please enter name';
+    }
+
+    // Validate password
+    if(empty($password)){
+      $password_err = 'Please enter password';
+    } elseif(strlen($password) < 6){
+      $password_err = 'Password must be at least 6 characters ';
+    }
+
+    // Validate Confirm password
+    if(empty($confirm_password)){
+      $confirm_password_err = 'Please confirm password';
+    } else {
+      if($password !== $confirm_password){
+        $confirm_password_err = 'Passwords do not match';
+      }
+    }
+
+    // Make sure errors are empty
+    if(empty($name_err) && empty($email_err) && empty($password_err) && empty($confirm_password_err)){
+      // Hash password
+      $password = password_hash($password, PASSWORD_DEFAULT);
+
+      // Prepare insert query
+      $sql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+      $result = mysqli_stmt_init($conn);
+       if (!mysqli_stmt_prepare($result, $sql)) {
+          echo "SQL_Error_Select_login1";
+          exit();
+        }
+        else{
+            mysqli_stmt_bind_param($result, "sss", $name, $email, $password);
+            mysqli_stmt_execute($result);
+            header('location: login.php');
+            exit();
+          }
+
+    }
+
+
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <title>Register An Account</title>
+</head>
+<body class="bg-primary">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 mx-auto">
+        <div class="card card-body bg-light mt-5">
+          <h2>Create Account</h2>
+          <p>Fill in this form to register</p>
+          <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+              <span class="invalid-feedback"><?php echo $name_err; ?></span>
+            </div>
+            <div class="form-group">
+              <label for="email">Email Address</label>
+              <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+              <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+              <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+              <label for="confirm_password">Confirm Password</label>
+              <input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+              <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+
+            <div class="form-row">
+              <div class="col">
+                <input type="submit" value="Register" class="btn btn-success btn-block">
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
